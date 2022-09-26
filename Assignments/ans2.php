17a13f5e9cb6291a489cf463f4f0feb044d5b1b3
Deepak Jogade <105729542+deepakjogade@users.noreply.github.com>
2022-09-27 01:54:32 +0530

    Add files via upload

---
+++ Assignments/ans2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+?>
+<p>// This is a single-line comment.</p>
+
+<P># This is a single-line comment.</p>
+<p>/*
+This is a multiple-lines comment block <br />
+that spans over multiple <br />
+lines
+*/
+</p>
+
+
+
+</body>
+</html>
+
---
+++ Assignments/ans4.php
@@ -0,0 +1,21 @@
+<?php
+$day= "sunday";
+
+switch($day){
+    case "monday":
+        echo"Today is not Holiday";
+        break;
+    case "tuesday":
+        echo"Today is not Holiday";
+    case "wednesday":
+        echo"Today is not Holiday";
+    case "thursday":
+        echo"Today is not Holiday";
+    case "friday":
+        echo"Today is not Holiday";
+    case "satureday":
+        echo"Today is not Holiday";
+    default:
+        echo"Today is Holiday";
+}
+?>
